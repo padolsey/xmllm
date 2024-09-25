c32996f89ae9d3bd43dc1a86502491774028ce44
James Padolsey <github@padolsey.net>
2024-09-25 12:25:05 -0700

    rampantly attempting to export types correctly

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,27 @@
-import { ClientProvider } from './src/xmllm-client';
-export { ClientProvider };
+// index.d.ts
 
-export { default as xmllm } from './src/xmllm-client';
+// Exporting the main 'xmllm' function
+export default function xmllm(pipelineFn: Function, options?: any): AsyncGenerator<any>;
 
-import xmllm from './src/xmllm-main';
-export default xmllm;
+// Module declaration for 'xmllm/client'
+declare module 'xmllm/client' {
+  // Importing from './src/xmllm-client' (adjust the path if necessary)
+  import { ClientProvider as CP } from './src/xmllm-client';
+
+  // Re-exporting ClientProvider
+  export class ClientProvider extends CP {}
+
+  // Exporting the 'xmllm' function
+  export function xmllm(
+    pipelineFn: Function,
+    clientProvider: ClientProvider | string,
+    options?: any
+  ): AsyncGenerator<any>;
+
+  // Default export
+  const defaultExport: {
+    ClientProvider: typeof ClientProvider;
+    xmllm: typeof xmllm;
+  };
+  export default defaultExport;
+}
---
+++ xmllm-client.d.ts
@@ -0,0 +1,8 @@
+// xmllm-client.d.ts
+
+import { ClientProvider } from './src/xmllm-client';
+export { ClientProvider };
+
+export { xmllm } from './src/xmllm-client';
+export default xmllm;
+
